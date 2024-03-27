2889eebdaf8035841e9c2b6644f7a0d21cb64312
kenot72 <tgolab72@gmail.com>
2024-03-27 10:20:31 +0100

    zad 2.2

--- virtual_dc_motor/scripts/current_position.py
+++ virtual_dc_motor/scripts/current_position.py
@@ -1,37 +1,20 @@
 #!/usr/bin/env python3
 import rospy
-import time
+
+from pyscript import document
+
 from std_msgs.msg import UInt16
 
-def callback0(msg: UInt16):
-    position = str(msg)
-    res = [int(i) for i in position.split() if i.isdigit()]
-    for x in res:
-        rospy.loginfo("pozycja 0:" + str(round(x/(4096/360),2)))
-    time.sleep(1)
-    
+tekst0 = document.querrySelector
 
-def callback1(msg: UInt16):
+def pose_callback(msg: UInt16):
     position = str(msg)
-    res = [int(i) for i in position.split() if i.isdigit()]
-    for x in res:
-        rospy.loginfo("pozycja 1:" + str(round(x/(4096/360),2)))
-    time.sleep(1)
-    
-def callback2(msg: UInt16):
-    position = str(msg)
-    res = [int(i) for i in position.split() if i.isdigit()]
-    for x in res:
-        rospy.loginfo("pozycja 2:" + str(round(x/(4096/360),2)) + "\n")
-    time.sleep(1)
-    
-    
+    rospy.loginfo("position: " + position)
+
 if __name__ == '__main__':
     rospy.init_node("current_position")
+
+    sub = rospy.Subscriber("/virtual_dc_motor_node/get_position_0", UInt16, callback=pose_callback)
+
     rospy.loginfo("subscriber is working")
-
-    sub0 = rospy.Subscriber("/virtual_dc_motor_node/get_position_0", UInt16, callback=callback0)
-    sub1 = rospy.Subscriber("/virtual_dc_motor_node/get_position_1", UInt16, callback=callback1)
-    sub2 = rospy.Subscriber("/virtual_dc_motor_node/get_position_2", UInt16, callback=callback2)
-
     rospy.spin()
